e00d3cf45e887bfe723b2f02c9ca303cd70f32f1
Qitian Zeng <qzeng3@hawk.iit.edu>
2018-02-28 13:06:31 -0600

    removed unimportant code

--- proc.c
+++ proc.c
@@ -6,7 +6,6 @@
 #include "x86.h"
 #include "proc.h"
 #include "spinlock.h"
-#include "syscall.h"
 
 struct {
   struct spinlock lock;
--- test.c
+++ test.c
@@ -4,7 +4,10 @@
 #include "syscall.h"
 
 int main(void){
-	write(1,"123\n",4);
-	printf(1,"write called %d times\n",getCallCount(SYS_write));
+	if (fork()==0){
+		printf(1,"fork called %d times in child process\n",getCallCount(SYS_fork));
+	}else{
+		printf(1,"fork called %d times in parent process\n",getCallCount(SYS_fork));
+	}
 	exit();
 }
