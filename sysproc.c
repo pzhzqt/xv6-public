70d585021200730fb9132c8092411f420db74f1d
Qitian Zeng <qzeng3@hawk.iit.edu>
2018-03-27 16:37:20 -0500

    Finished syscall. Needs documentation.

--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,32 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+//print the number of pages allocated
+//return 0 on success
+int
+sys_mem(void)
+{
+	pte_t *pgtab;
+	struct proc *proc=myproc();
+	int num_page=0,num_user=0;
+
+	for (int i=0;i<NPDENTRIES;i++){
+		if (proc->pgdir[i] & PTE_P){
+			pgtab = (pte_t *)P2V(PTE_ADDR(proc->pgdir[i]));
+			for (int j=0;j<NPTENTRIES;j++){
+				if (pgtab[j] & PTE_P){
+					num_page++;
+					if((pgtab[j] & PTE_U) && (pgtab[j] & PTE_W)){
+						num_user++;
+					}
+				}
+			}
+		}
+	}
+
+	cprintf("number of total pages: %d\n",num_page);
+	cprintf("number of user accessible and writable pages: %d\n",num_user);
+
+	return 0;
+}
